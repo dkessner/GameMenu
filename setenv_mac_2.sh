c4f76fab8fccfe52f77aad58fd026c2ee98f2e37
Darren Kessner <Darren.Kessner@marlborough.org>
2025-04-29 14:45:38 -0700

    new file:   setenv_mac_2.sh

---
+++ setenv_mac_2.sh
@@ -0,0 +1,3 @@
+export CLASSPATH=".:/Applications/Processing.app/Contents/Java/core/library/*"
+export CLASSPATH="$HOME/Documents/Processing4/libraries/sound/library/*:$CLASSPATH"
+export CLASSPATH="$HOME/Documents/Processing4/libraries/minim/library/*:$CLASSPATH"
